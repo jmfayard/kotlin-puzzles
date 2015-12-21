82f722fafaf862b39de665c3a1ce3a0163f02ff0
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2015-12-21 09:26:08 +0100

    Parser done

--- kotlin/adventofcode/day7/day7.kt
+++ kotlin/adventofcode/day7/day7.kt
@@ -10,16 +10,7 @@ infix fun Int.AND(other: Int): Int = this and other
 infix fun Int.OR(other: Int): Int = this or other
 fun NOT(a: Int): Int = a.inv()
 
-/**
-123 -> x
-456 -> y
-x AND y -> d
-x OR y -> e
-x LSHIFT 2 -> f
-y RSHIFT 2 -> g
-NOT x -> h
-NOT y -> i
-**/
+
 fun main(args: Array<String>) {
     val instructions = ArrayList<List<Lex>>()
     val testing = false
@@ -35,7 +26,74 @@ fun main(args: Array<String>) {
     if (errors.isEmpty()) println("No error found")
     else println(errors.joinToString(separator = "\n"))
 
+    println("\n== Parser ==")
+    println(buildCircuit(instructions).joinToString(separator = "\n"))
+
 }
+
+val circuit = object : Map<String, BitInstruction> by HashMap<String, BitInstruction>() {
+
+}
+
+fun buildCircuit(instructions: List<List<Lex>>): List<BitInstruction> {
+    val result = instructions.map { instruction ->
+        val last = instruction[instruction.lastIndex] as LexVariable
+        if (instruction.size == 3) { // leaf
+            val token = instruction[0] as LexExpression
+            BitInstruction(
+                    first = token,
+                    second = null,
+                    operator = null,
+                    key = last.name)
+
+        } else if (instruction.size == 4) {
+            val token = instruction[1] as LexExpression
+            BitInstruction(
+                    first = token,
+                    second = null,
+                    operator = LexGate.NOT,
+                    key = last.name)
+        } else {
+            val operator = instruction[1] as LexGate
+            val firstToken = instruction[0] as LexExpression
+            val secondToken = instruction[2] as LexExpression
+            BitInstruction(
+                    first = firstToken,
+                    second = secondToken,
+                    operator = operator,
+                    key = last.name)
+        }
+    }
+    return result
+}
+
+fun validateAndSortCircuit() {
+
+}
+
+fun Lex.tokenName(): String =
+    if (this is LexVariable) name
+    else ""
+
+
+fun Lex.tokenValue(): Int? =
+    if (this is LexValue) value
+    else null
+
+class BitInstruction(val key : String, val value: Int? = null, val operator: LexGate?, val first: LexExpression?, val second : LexExpression?) {
+    fun isComputed() : Boolean = (value != null)
+    fun dependancies(): List<String> {
+        val result = ArrayList<String>()
+        if (first != null && first is LexVariable)
+            result.add(first.name)
+        if (second != null && second is LexVariable)
+            result.add(second.name)
+        return result
+    }
+
+    override fun toString(): String = "BitInstruction( key=$key, value=$value, depandancies=${dependancies()} operator=$operator first=$first second=$second"
+}
+
 
 data class TokenError(val line : Int, val instruction : List<Lex>, val message : String)
 fun validateInstructions(instructions: List<List<Lex>>) : List<TokenError> {
@@ -56,11 +114,11 @@ fun validateInstructions(instructions: List<List<Lex>>) : List<TokenError> {
             errorMessage = "Missing asignment"
         else
             errorMessage = when(instruction.size) {
-                3 -> if (first is LexValue || first is LexVariable) "" else "Invalid assignment"
+                3 -> if (first is LexExpression) "" else "Invalid assignment"
                 4 -> if (first == LexGate.NOT) "" else "Not an unary operator"
                 5 -> if (second == LexGate.NOT) "Not an binary operator"
-                    else if (!(first is LexValue || first is LexVariable)) "Invalid first operand"
-                    else if (!(third is LexValue || third is LexVariable)) "Invalid second operand"
+                    else if (!(first is LexExpression)) "Invalid first operand"
+                    else if (!(third is LexExpression)) "Invalid second operand"
                     else ""
                 else -> "No instruction are allowed with ${instruction.size} elements"
             }
@@ -93,26 +151,19 @@ fun emitLine(line: String) : List<Lex> {
 var regexp_isNumber = Regex(pattern = "[0-9]+")
 fun isNumber(word: String): Boolean = regexp_isNumber.matches(word)
 
-val circuit = object : Map<String, BitInstruction> by HashMap<String, BitInstruction>() {
 
-}
 
 interface Lex {}
-data class LexValue(val value: Int) : Lex
-data class LexVariable(val name: String) : Lex
+interface LexExpression : Lex
+data class LexValue(val value: Int) : LexExpression
+data class LexVariable(val name: String) : LexExpression
 class LexAssignment() : Lex {
     override fun toString(): String = " -> "
 }
 enum class LexGate : Lex {
     AND, OR, NOT, LSHIFT, RSHIFT ;
-
 }
 
 
 
-data class BitInstruction(val text : String) {
-    init {
-
-    }
-}
 
